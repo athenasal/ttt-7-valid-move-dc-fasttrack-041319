4a3810b51d69576a62e0fe8d94ae0a67f2a8c698
Athena Salomons <athena.asalomons@gmail.com>
2019-04-14 14:30:34 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -11,4 +11,8 @@ board [7]
 board [8]
 
 if board.between(0,8)
+  puts board
+else
+  return "Enter a valid number."
+  
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
